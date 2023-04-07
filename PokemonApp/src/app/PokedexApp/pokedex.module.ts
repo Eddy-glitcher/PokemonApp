dff7b50448b169f754b8667f15977026245ae988
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-07 14:22:08 -0500

    Feat: Added the header component to the layout

--- PokemonApp/src/app/PokedexApp/pokedex.module.ts
+++ PokemonApp/src/app/PokedexApp/pokedex.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PokedexRoutingModule } from './pokedex-routing.module';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     PokedexRoutingModule
+  ],
+  exports:[
+    HeaderComponent
   ]
 })
 export class PokedexModule { }
