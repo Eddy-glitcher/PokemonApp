bd840f3a431b4b994b1f820652a31bfe89a78dbc
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-06 19:44:03 -0500

    Feat: created the file pokedex whit the principal module and rounting

---
+++ PokemonApp/src/app/PokedexApp/pokedex.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PokedexRoutingModule } from './pokedex-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PokedexRoutingModule
+  ]
+})
+export class PokedexModule { }
