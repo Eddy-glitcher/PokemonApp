6239d6a081a1653ef98d7786bc6fe758979031ed
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-07 19:35:32 -0500

    Feat: Added the pokemon-filters component to the layout

--- PokemonApp/src/app/PokedexApp/pokedex.module.ts
+++ PokemonApp/src/app/PokedexApp/pokedex.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { PokedexRoutingModule } from './pokedex-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
+import { PokemonFiltersComponent } from './components/pokemon-filters/pokemon-filters.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    PokemonFiltersComponent
   ],
   imports: [
     CommonModule,
     PokedexRoutingModule
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    PokemonFiltersComponent
   ]
 })
 export class PokedexModule { }
