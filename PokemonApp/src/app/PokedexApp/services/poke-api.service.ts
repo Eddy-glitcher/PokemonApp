1096f50446cd863eeb4376324de5c8be83202cc0
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-12 20:51:50 -0500

    Feat: Added the environments config for the aplicarion

--- PokemonApp/src/app/PokedexApp/services/poke-api.service.ts
+++ PokemonApp/src/app/PokedexApp/services/poke-api.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { PokemonData } from '../interfaces/pokemon-data';
+import { PokemonList } from '../interfaces/pokemon-list';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
-  constructor() { }
+
+  baseUrl = environment.baseUrl;
+  pokemonInitialList : PokemonData[] = [];
+
+  constructor(private http : HttpClient) {
+    this.getInitialPokemons();
+  }
+
+  getInitialPokemons(){
+    const url :string = `${this.baseUrl}/pokemon/`
+
+    this.http.get<PokemonList>(url).subscribe( (resp) =>{
+      console.log(resp);
+    })
+  }
+
+
 }
