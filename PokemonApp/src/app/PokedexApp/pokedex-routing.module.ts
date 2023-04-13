b0266b2bedb91847994cc76d48bce068f1c50b0e
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-12 21:15:24 -0500

    Feat: Added the routing and lazzyload functionality

--- PokemonApp/src/app/PokedexApp/pokedex-routing.module.ts
+++ PokemonApp/src/app/PokedexApp/pokedex-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:'home',
+        component: HomePageComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'home'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- PokemonApp/src/app/app-routing.module.ts
+++ PokemonApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Pokedex', loadChildren: ()=> import('./PokedexApp/pokedex.module').then(m => m.PokedexModule)},
+  {path: '**', redirectTo: 'Pokedex'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
