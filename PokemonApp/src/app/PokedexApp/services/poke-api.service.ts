2380d1ff37c3bdb51bbb6ceaf1e3a5606ed2c019
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-14 19:37:23 -0500

    Feat: Added the pokeapi service to get initial pokemons

--- PokemonApp/src/app/PokedexApp/services/poke-api.service.ts
+++ PokemonApp/src/app/PokedexApp/services/poke-api.service.ts
@@ -4,34 +4,50 @@ import { HttpClient } from '@angular/common/http';
 import { PokemonData } from '../interfaces/pokemon-data';
 import { PokemonList } from '../interfaces/pokemon-list';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  baseUrl = environment.baseUrl;
-  pokemonInitialList : PokemonData[] = [];
+baseUrl = environment.baseUrl;
+pokemonInitialList : PokemonData[] = [];
 
-  constructor(private http : HttpClient) {
-  }
+constructor(private http : HttpClient) {
+}
 
-  getPokemonByUrl(url: string) {
-    return this.http.get<PokemonData>(url);
-  }
+getPokemonByUrl(url: string) {
+  return this.http.get<PokemonData>(url);
+}
 
-  getInitialPokemons(){
-    const url :string = `${this.baseUrl}/pokemon/`
+getInitialPokemons(){
+  const url :string = `${this.baseUrl}/pokemon/`
 
-    this.http.get<PokemonList>(url).subscribe( (resp) =>{
-      this.pokemonInitialList = [];
-        resp.results.forEach((pokemon) => {
-          this.getPokemonByUrl(pokemon.url).subscribe((resp) => {
-            this.pokemonInitialList.push(resp);
-          });
+  this.http.get<PokemonList>(url).subscribe( (resp) =>{
+    this.pokemonInitialList = [];
+      resp.results.forEach((pokemon) => {
+        this.getPokemonByUrl(pokemon.url).subscribe((resp) => {
+          this.pokemonInitialList.push(resp);
         });
-      })
-  }
+      });
+    })
+}
 
+get initialPokemons(): PokemonData[] {
+  this.sortedPokemonList(this.pokemonInitialList);
+  return this.pokemonInitialList;
+}
+
+sortedPokemonList(arr: PokemonData[]){
+  arr.sort((a, b) => {
+    if (a.id > b.id) {
+      return 1;
+    }
+    if (a.id < b.id) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+}
 
 }
