025059b5e7e4a11f143de6a91ae291d49263700e
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-14 19:48:21 -0500

    Feat: import home-page and pokemon-card components

--- PokemonApp/src/app/PokedexApp/pokedex.module.ts
+++ PokemonApp/src/app/PokedexApp/pokedex.module.ts
@@ -5,13 +5,17 @@ import { PokedexRoutingModule } from './pokedex-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { PokemonFiltersComponent } from './components/pokemon-filters/pokemon-filters.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    PokemonFiltersComponent
+    PokemonFiltersComponent,
+    HomePageComponent,
+    PokemonCardComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +24,9 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
   ],
   exports:[
     HeaderComponent,
-    PokemonFiltersComponent
+    PokemonFiltersComponent,
+    HomePageComponent,
+    PokemonCardComponent
   ]
 })
 export class PokedexModule { }
