b24306200c01690604760e907dcf3a5b50ac78d0
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-04-20 20:59:14 -0500

    Feat: Added the progress-bars functionality

--- PokemonApp/src/app/PokedexApp/pages/pokemon-view/pokemon-view.component.ts
+++ PokemonApp/src/app/PokedexApp/pages/pokemon-view/pokemon-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { PokeApiService } from '../../services/poke-api.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
@@ -9,18 +9,44 @@ import { PokemonData } from '../../interfaces/pokemon-data';
   templateUrl: './pokemon-view.component.html',
   styleUrls: ['./pokemon-view.component.scss']
 })
-export class PokemonViewComponent implements OnInit {
+export class PokemonViewComponent implements OnInit, AfterViewChecked {
+
+  // To set the pokemon object
   pokemon: PokemonData[] = [];
 
-  constructor( private pokeService: PokeApiService, private activatedRoute: ActivatedRoute){}
+  // To get the html alias list
+  @ViewChildren('stat') statElements!: QueryList<ElementRef<HTMLElement>>;
+
+  constructor( private pokeService: PokeApiService, private activatedRoute: ActivatedRoute, private renderer2 : Renderer2){}
 
   ngOnInit(): void {
+    // To get the pokemon by Id from pokeapi service, using the url params
     this.activatedRoute.params.pipe(
-    switchMap( ({id}) => this.pokeService.getPokeomnById(Number(id)))
-    ).subscribe(pokemon => {
-      console.log(pokemon);
+      switchMap( ({id}) => this.pokeService.getPokeomnById(Number(id)))
+      ).subscribe(pokemon => {
         this.pokemon = [];
         this.pokemon.push(pokemon);
-    });
+      });
+  }
+
+  ngAfterViewChecked(): void {
+
+    // To set the width for the html progress-bars whit renderer2
+    const setWidth= (statIndex:number, width:number) =>{
+      if (this.statElements && this.statElements.length > 0) {
+        this.statElements.forEach((stat, index) => {
+          if (index == statIndex) {
+            this.renderer2.setStyle(stat.nativeElement, 'width', `${width}px`);
+          }
+        })
+      }
+    }
+
+    // To loop through a pokemon stats and generate the progress-bars styles
+    if (this.pokemon.length > 0) {
+      this.pokemon[0].stats.forEach((stat,index)=>{
+        setWidth(index,stat.base_stat);
+      });
+    }
   }
 }
